a6112fa6bf298a73e56ee0053cd842481ed835d9
Brilliant Kwakye <kwakyebrilliant@gmail.com>
2024-02-06 13:55:27 +0000

    tutorial-3 commits

--- lib/main.dart
+++ lib/main.dart
@@ -1,57 +1,22 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(CounterApp());
+  runApp(const MyApp());
 }
 
-// class MyApp extends StatelessWidget {
-//   const MyApp({super.key});
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
 
-//   @override
-//   Widget build(BuildContext context) {
-//     return MaterialApp(
-//       home: Scaffold(
-//         appBar: AppBar(
-//           title: const Text('Hello World App'),
-//         ),
-//         body: const Center(
-//           child: Text('Hello World'),
-//         ),
-//       ),
-//     );
-//   }
-// }
-
-class CounterApp extends StatefulWidget {
-  @override
-  _CounterAppState createState() => _CounterAppState();
-}
-
-class _CounterAppState extends State<CounterApp> {
-  int count = 0;
-
+  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
-          title: const Text('Counter App'),
+          title: const Text('Hello World App'),
         ),
-        body: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Text('Count: $count'),
-              ElevatedButton(
-                onPressed: () {
-                  setState(() {
-                    count++;
-                  });
-                },
-                child: Text('Increment'),
-              ),
-            ],
-          ),
+        body: const Center(
+          child: Text('Hello Wolrd!'),
         ),
       ),
     );
