83f17245f374394cfdaa6d91e8f5cb39e51ba725
Brilliant Kwakye <kwakyebrilliant@gmail.com>
2024-02-12 09:15:07 +0000

    updates

--- lib/main.dart
+++ lib/main.dart
@@ -7,15 +7,112 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
+  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
-          title: const Text('Hello World App'),
+          title: const Text('Flutter Layout'),
         ),
-        body: const Center(
-          child: Text('Hello Wolrd!'),
+        body: Column(
+          children: [
+            //top container
+            Center(
+              child: Container(
+                height: 170.0,
+                width: 320.0,
+                color: Colors.blue,
+              ),
+            ),
+
+            //first row
+            Padding(
+              padding: const EdgeInsets.fromLTRB(35.0, 35.0, 35.0, 35.0),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  //first container
+                  Container(
+                    height: 50.0,
+                    width: 140.0,
+                    color: Colors.blue,
+                  ),
+
+                  //second container
+                  Container(
+                    height: 50.0,
+                    width: 140.0,
+                    color: Colors.blue,
+                  ),
+                ],
+              ),
+            ),
+
+            //second row
+            Padding(
+              padding: const EdgeInsets.fromLTRB(35.0, 0.0, 35.0, 35.0),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  //first container
+                  Container(
+                    height: 50.0,
+                    width: 140.0,
+                    color: Colors.blue,
+                  ),
+
+                  //second container
+                  Container(
+                    height: 50.0,
+                    width: 140.0,
+                    color: Colors.blue,
+                  ),
+                ],
+              ),
+            ),
+
+            //third row
+            Padding(
+              padding: const EdgeInsets.fromLTRB(35.0, 0.0, 35.0, 35.0),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  //first container
+                  Container(
+                    height: 280.0,
+                    width: 140.0,
+                    color: Colors.blue,
+                  ),
+
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      //second container
+                      Padding(
+                        padding: const EdgeInsets.only(bottom: 20.0),
+                        child: Container(
+                          height: 140.0,
+                          width: 140.0,
+                          color: Colors.blue,
+                        ),
+                      ),
+
+                      //third container
+                      Padding(
+                        padding: const EdgeInsets.only(top: 20.0),
+                        child: Container(
+                          height: 100.0,
+                          width: 140.0,
+                          color: Colors.blue,
+                        ),
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+            ),
+          ],
         ),
       ),
     );
