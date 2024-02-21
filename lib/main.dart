1b031178119ba9a0fc91fba5118a747ab68f8f0b
Brilliant Kwakye <kwakyebrilliant@gmail.com>
2024-02-21 08:33:56 +0000

    tutorial-8 commit

--- lib/main.dart
+++ lib/main.dart
@@ -10,12 +10,49 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          title: const Text('Hello World App'),
+      initialRoute: '/',
+      routes: {
+        '/': (context) => FirstScreen(),
+        '/second': (context) => SecondScreen(),
+      },
+    );
+  }
+}
+
+class FirstScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('First Screen'),
+      ),
+      body: Center(
+        child: ElevatedButton(
+          onPressed: () {
+            //Navigate to second screen using named route
+            Navigator.pushNamed(context, '/second');
+          },
+          child: const Text('Go To Second Screen'),
         ),
-        body: const Center(
-          child: Text('Hello Wolrd!'),
+      ),
+    );
+  }
+}
+
+class SecondScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Second Screen'),
+      ),
+      body: Center(
+        child: ElevatedButton(
+          onPressed: () {
+            //Navigate back to the first screen
+            Navigator.pop(context);
+          },
+          child: const Text('Go Back To First Screen'),
         ),
       ),
     );
