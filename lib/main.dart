e1e4af1d09404b6ced0d0ee3b9e43056effea34f
Brilliant Kwakye <kwakyebrilliant@gmail.com>
2024-03-05 15:34:16 +0000

    tutorial 9

--- lib/main.dart
+++ lib/main.dart
@@ -9,14 +9,94 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          title: const Text('Hello World App'),
+    return const MaterialApp(
+      home: DrawerNavigationExample(),
+    );
+  }
+}
+
+class DrawerNavigationExample extends StatelessWidget {
+  const DrawerNavigationExample({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Drawer Navigation"),
+      ),
+      drawer: Drawer(
+        child: ListView(
+          padding: EdgeInsets.zero,
+          children: [
+            const DrawerHeader(
+              child: Text("Drawer Header"),
+              decoration: BoxDecoration(
+                color: Colors.green,
+              ),
+            ),
+            ListTile(
+              title: const Text("Home"),
+              onTap: () {
+                Navigator.pop(context);
+              },
+            ),
+            ListTile(
+              title: const Text("Settings"),
+              onTap: () {
+                Navigator.pop(context);
+              },
+            ),
+          ],
         ),
-        body: const Center(
-          child: Text('Hello Wolrd!'),
-        ),
+      ),
+    );
+  }
+}
+
+class BottomNavBarExample extends StatefulWidget {
+  const BottomNavBarExample({super.key});
+
+  @override
+  State<BottomNavBarExample> createState() => _BottomNavBarExampleState();
+}
+
+class _BottomNavBarExampleState extends State<BottomNavBarExample> {
+  int _currentIndex = 0;
+
+  final List<Widget> _children = [
+    const Scaffold(
+      backgroundColor: Colors.blueGrey,
+    ),
+    const Scaffold(
+      backgroundColor: Colors.green,
+    ),
+    const Scaffold(
+      backgroundColor: Colors.orange,
+    ),
+  ];
+
+  void onTapTapped(int index) {
+    setState(() {
+      _currentIndex = index;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Bottom Navbar"),
+      ),
+      body: _children[_currentIndex],
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: _currentIndex,
+        onTap: onTapTapped,
+        items: const [
+          BottomNavigationBarItem(icon: Icon(Icons.home), label: "Home"),
+          BottomNavigationBarItem(icon: Icon(Icons.search), label: "Search"),
+          BottomNavigationBarItem(
+              icon: Icon(Icons.settings), label: "Settings"),
+        ],
       ),
     );
   }
