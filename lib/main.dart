4ef5b1d71529c02a078158b187db2b59b4ed8279
Brilliant Kwakye <kwakyebrilliant@gmail.com>
2024-03-08 18:47:38 +0000

    tutorial-10 updates

--- lib/main.dart
+++ lib/main.dart
@@ -4,18 +4,44 @@ void main() {
   runApp(const MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   const MyApp({super.key});
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  int _counter = 0;
+
+  void _incrementCounter() {
+    setState(() {
+      _counter++;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
-          title: const Text('Hello World App'),
+          title: const Text('State Management'),
         ),
-        body: const Center(
-          child: Text('Hello Wolrd!'),
+        body: Center(
+          child: Column(
+            children: [
+              const Text('Counter:'),
+              Text(
+                '$_counter',
+                style: Theme.of(context).textTheme.headlineMedium,
+              ),
+            ],
+          ),
+        ),
+        floatingActionButton: FloatingActionButton(
+          onPressed: _incrementCounter,
+          tooltip: 'Increement',
+          child: const Icon(Icons.add_rounded),
         ),
       ),
     );
