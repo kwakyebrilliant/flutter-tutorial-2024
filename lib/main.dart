493787e3849753d3b074681a0870ac4928aa30b2
Brilliant Kwakye <kwakyebrilliant@gmail.com>
2024-03-11 09:56:42 +0000

    forms

--- lib/main.dart
+++ lib/main.dart
@@ -9,13 +9,115 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          title: const Text('Hello World App'),
-        ),
-        body: const Center(
-          child: Text('Hello Wolrd!'),
+    return const MaterialApp(
+      home: MyForm(),
+    );
+  }
+}
+
+class MyForm extends StatefulWidget {
+  const MyForm({super.key});
+
+  @override
+  State<MyForm> createState() => _MyFormState();
+}
+
+class _MyFormState extends State<MyForm> {
+  final _formKey = GlobalKey<FormState>();
+  String _name = '';
+  String _password = '';
+  bool _isChecked = false;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Flutter Form'),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Form(
+          key: _formKey,
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              //name textformfield
+              TextFormField(
+                decoration: const InputDecoration(
+                  labelText: 'Name',
+                  hintText: 'Enter your name',
+                ),
+                validator: (value) {
+                  if (value!.isEmpty) {
+                    return 'Please enter your name';
+                  }
+                  return null;
+                },
+                onSaved: (value) {
+                  _name = value!;
+                },
+              ),
+
+              const SizedBox(
+                height: 16.0,
+              ),
+
+              //password textformfield
+              TextFormField(
+                decoration: const InputDecoration(
+                  labelText: 'Password',
+                  hintText: 'Enter your password',
+                ),
+                obscureText: true,
+                validator: (value) {
+                  if (value!.isEmpty) {
+                    return 'Please enter your password';
+                  }
+                  return null;
+                },
+                onSaved: (value) {
+                  _password = value!;
+                },
+              ),
+
+              const SizedBox(
+                height: 16.0,
+              ),
+
+              //checkbox here
+              Row(
+                children: [
+                  Checkbox(
+                      value: _isChecked,
+                      onChanged: (value) {
+                        setState(() {
+                          _isChecked = value!;
+                        });
+                      }),
+                  const Text('I agree to the terms and conditions'),
+                ],
+              ),
+
+              const SizedBox(
+                height: 16.0,
+              ),
+
+              //button here
+              ElevatedButton(
+                onPressed: () {
+                  if (_formKey.currentState!.validate()) {
+                    _formKey.currentState!.save();
+
+                    //Process the form data
+                    print('Name: $_name');
+                    print('Password: $_password');
+                    print('Agreed to terms: $_isChecked');
+                  }
+                },
+                child: const Text('Submit'),
+              ),
+            ],
+          ),
         ),
       ),
     );
