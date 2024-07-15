ddb20d9e0c4eecde356a927895a33e01afba00ac
Brilliant Kwakye <kwakyebrilliant@gmail.com>
2024-07-15 13:04:57 +0000

    http request

--- lib/main.dart
+++ lib/main.dart
@@ -1,22 +1,112 @@
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
+import 'package:intl/intl.dart';
 
 void main() {
   runApp(const MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   const MyApp({super.key});
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  Map<String, dynamic>? data;
+  String? date;
+  String? time;
+
+  @override
+  void initState() {
+    super.initState();
+    fetchData();
+  }
+
+  void fetchData() async {
+    // Convert URL string to Uri object
+    Uri url = Uri.parse('http://worldtimeapi.org/api/timezone/Africa/Accra');
+    final response = await http.get(url);
+    if (response.statusCode == 200) {
+      setState(() {
+        data = jsonDecode(response.body);
+
+        // Parse datetime string
+        DateTime dateTime = DateTime.parse(data!['datetime']);
+
+        // Format the date and time line separately
+        date = '${dateTime.year}-${dateTime.month}-${dateTime.day}';
+        time = DateFormat('hh : mm a').format(dateTime);
+      });
+    } else {
+      // Handle Error
+      print('Error: ${response.statusCode}');
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
-          title: const Text('Hello World App'),
+          title: const Text('WorldTime API'),
         ),
-        body: const Center(
-          child: Text('Hello Wolrd!'),
-        ),
+        body: data == null
+            ? const Center(
+                child: CircularProgressIndicator(
+                color: Colors.black,
+              ))
+            : Padding(
+                padding: EdgeInsets.all(16.0),
+                child: Center(
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Text(
+                        '${data!['timezone']}',
+                        style: const TextStyle(
+                          fontSize: 28.0,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                      const SizedBox(
+                        height: 5.0,
+                      ),
+                      Text('$date'),
+                      const SizedBox(
+                        height: 5.0,
+                      ),
+                      Container(
+                        padding: const EdgeInsets.all(12.0),
+                        decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(8.0),
+                            color: Colors.white,
+                            border: Border.all(
+                              color: Colors.orangeAccent,
+                              width: 1.0,
+                            ),
+                            boxShadow: const [
+                              BoxShadow(
+                                color: Colors.grey,
+                                spreadRadius: 0,
+                                blurRadius: 5,
+                                offset: Offset(0, 2),
+                              )
+                            ]),
+                        child: Text(
+                          '$time',
+                          style: const TextStyle(
+                            fontSize: 28.0,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
       ),
     );
   }
